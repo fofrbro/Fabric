9c629dfecbd68a2840683c3bfa06d17c2957d3c6
Cheikhou Fofana <cheikhou_fofana@live.fr>
2026-05-14 07:51:16 +0200

    Committing 5 items from workspace 47cd485c-fcd8-4d85-90bd-17ab010c39a9

--- DW_COPILOT.Warehouse/rls/Functions/fn_security.sql
+++
@@ -1,9 +0,0 @@
-;
--- Fonction de sécurité
-CREATE FUNCTION rls.fn_security(@SalesRep AS VARCHAR(60))
-    RETURNS TABLE
-WITH SCHEMABINDING
-AS
-    RETURN
-        SELECT 1 AS fn_security_result
-    WHERE @SalesRep = USER_NAME()
